078c25275ce8af4e31b88f1fde4bf5847d75a753
Subhash Singh <ss5138275@gmail.com>
2025-09-01 23:38:44 +0545

    project

--- src/constants.js
+++ src/constants.js
@@ -39,6 +39,14 @@ import lotusLogo from './assets/education_logo/lotus.png'
 import arnikoLogo from './assets/education_logo/arniko.png'
 import kuLogo from './assets/education_logo/ku.png'
 
+// project logo
+import eStoreLogo from './assets/project/e-store.png'
+import sewaMitraLogo from './assets/project/sewamitra.png'
+
+
+
+
+
 export const SkillsInfo = [
   {
     title: "Frontend",
@@ -125,3 +133,125 @@ export const education = [
     degree: "NEB(X)",
   },
 ];
+
+
+
+  export const projects = [
+    {
+      id: 0,
+      title: "E-Store",
+      description:
+        "An e-store website is an online platform that allows businesses and individuals to buy and sell products or services over the internet. It provides features like product listings, shopping carts, secure payment gateways, and order tracking. E-commerce websites make shopping convenient, accessible 24/7, and often offer a wide variety of products at competitive prices.",
+      image: eStoreLogo,
+      tags: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "API",
+        "mongoDb",
+        "Stripe",
+      ],
+      github: "https://github.com/Suvassingh/E-commerce.git",
+      webapp: "https://emptymind.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "SewaMitra",
+      description:
+        "A service provider app is a platform that connects customers with professionals or businesses, enabling easy booking, payment, and delivery of services like home care, or repairs—all in one place.",
+      image: sewaMitraLogo,
+      tags: [
+        "Flutter",
+        "API",
+        "Firebase"
+      ],
+      github: "https://github.com/Suvassingh/SEWAMITRA1.git",
+      webapp: "",
+      apk:"/public/app-release.apk"
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: vercelLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "",
+
+    },
+    {
+      id: 3,
+      title: "Email Validator NPM Package",
+      description:
+        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+      image: vercelLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/codingmastr/cmtk-email-validator",
+      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: vercelLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: vercelLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Coding Master",
+      description:
+        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+      image: vercelLogo,
+      tags: [
+        "React JS",
+        "Node.js",
+        "MongoDB",
+        "Express",
+        "Payment Integration",
+      ],
+      github: "https://codingmasterweb.in/",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: vercelLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: vercelLogo,
+      tags: [
+        "React JS",
+        "API",
+        "Image Processing",
+        "HTML",
+        "CSS",
+        "Javascript",
+      ],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];
